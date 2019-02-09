f94c65cc5a813625ee77c3feeef6a0d381cd234a
km2717 <km2717@ic.ac.uk>
2019-02-09 19:35:28 +0000

    added SymbolTable Type Classes, and removed MainExample

---
+++ src/frontend/SymbolTable/Types/Pair.java
@@ -0,0 +1,51 @@
+package frontend.SymbolTable.Types;
+
+public class Pair extends Type {
+    private final Type first;
+    private final Type second;
+
+    public Pair(Type fst, Type snd) {
+        this.first = fst;
+        this.second = snd;
+    }
+
+    public Type getFirst() {
+        return first;
+    }
+
+    public Type getSecond() {
+        return second;
+    }
+
+    @Override
+    public boolean equals(Type other) {
+        if (other == this){
+            return true;
+        }
+
+        if (other instanceof InnerPair){
+            return true;
+        }
+
+        if (other instanceof Pair){
+            Pair pair = (Pair) other;
+
+            return checkEachElement(first, pair.first) && checkEachElement(second, pair.second);
+        }
+
+        //obj not an instance of pair
+        return false;
+    }
+
+    private boolean checkEachElement(Type x, Type y) {
+        return x == null || y == null || x.equals(y);
+    }
+
+    @Override
+    public String toString() {
+        String a = first == null ? "pair" : first.toString();
+        String b = second == null ? "pair" : second.toString();
+
+        return "pair(" + a + ", " + b + ")";
+    }
+}
