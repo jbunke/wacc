fe0206432bea276f7d6b3d27ecfadcd698921f03
Jordan Bunke <jtb17@ic.ac.uk>
2019-02-11 14:01:00 +0000

    Cleaned up BinaryOpExpressionNode file
     - Removed switch statement string to operator matching and replaced it with a static map
     - Added a value field to the OperatorType enum that reflects argument and return types

--- src/frontend/SymbolTable/Variable.java
+++ src/frontend/SymbolTable/Variable.java
@@ -3,7 +3,7 @@ package frontend.SymbolTable;
 
 import frontend.SymbolTable.Types.Type;
 
-public class Variable extends Identifier{
+public class Variable extends Identifier {
     private final Type type;
 
     public Variable(Type type) {
