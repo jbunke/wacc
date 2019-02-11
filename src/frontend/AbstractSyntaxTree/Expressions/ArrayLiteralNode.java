5e7efd9f1798030f651a369beba5262c5afccee1
km2717 <km2717@ic.ac.uk>
2019-02-11 13:20:16 +0000

    Completed TODO Implement ArrayLiteralNode, with semantic checking

--- src/frontend/AbstractSyntaxTree/Expressions/ArrayLiteralNode.java
+++ src/frontend/AbstractSyntaxTree/Expressions/ArrayLiteralNode.java
@@ -1,17 +1,55 @@
 package frontend.AbstractSyntaxTree.Expressions;
 
+
+import frontend.SymbolTable.SemanticError;
 import frontend.SymbolTable.SemanticErrorList;
 import frontend.SymbolTable.SymbolTable;
+import frontend.SymbolTable.Types.Array;
+import frontend.SymbolTable.Types.BaseTypes;
 import frontend.SymbolTable.Types.Type;
-//TODO Implement ArrayLiteral Node
-public class ArrayLiteralNode extends ExpressionNode{
+import frontend.SymbolTable.Variable;
+
+import java.util.List;
+
+public class ArrayLiteralNode extends ExpressionNode {
+    private final IdentifierNode identifier;
+    private final List<ExpressionNode> indices;
+
+    public ArrayLiteralNode(List<ExpressionNode> indices, IdentifierNode identifier) {
+        this.indices = indices;
+        this.identifier = identifier;
+    }
+
     @Override
     public void semanticCheck(SymbolTable symbolTable, SemanticErrorList errorList) {
+        for (ExpressionNode node : indices) {
+            final Type nodeType = node.getType(symbolTable);
+            if (!nodeType.equals(new BaseTypes(BaseTypes.base_types.INT))) {
+                errorList.addError(new SemanticError(
+                        "Error: Index used for array is not an integer!" + "" +
+                                "Expression supplied was of type \"" + nodeType.toString() + "\"."
+                ));
+            }
+        }
 
+        final Type identifierType = identifier.getType(symbolTable);
+        if (!(identifierType instanceof Array)) {
+            errorList.addError(new SemanticError(
+                    "Error: Tried to index a variable of non-array type. " +
+                            "Variable is of type \"" + identifierType.toString() + "\"."
+            ));
+        }
     }
 
     @Override
     public Type getType(SymbolTable symbolTable) {
-        return null;
+        Variable array = (Variable) symbolTable.fetchType(identifier.getId());
+        Array current = (Array) array.getType();
+
+        for (int i = indices.size(); i > 1; i--) {
+            current = (Array) current.getElementType();
+        }
+
+        return current.getElementType();
     }
 }
