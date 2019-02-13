adf42ce0be8af4adac3fa49a6f5efc74686e9a95
Patrick Henderson <pathend98@gmail.com>
2019-02-13 01:26:33 +0000

    Adding error listeners into the main file

--- src/WACCCompiler.java
+++ src/WACCCompiler.java
@@ -1,16 +1,20 @@
+import antlr.RecognitionException;
 import antlr.WACCLexer;
 import antlr.WACCParser;
 
 import org.antlr.v4.runtime.CharStream;
 import org.antlr.v4.runtime.CharStreams;
 import org.antlr.v4.runtime.CommonTokenStream;
+import org.antlr.v4.runtime.TokenStream;
 
 import java.io.IOException;
 
 public class WACCCompiler {
 
     private static final int EXPECTED_NUM_ARGS = 1;
+
     private static final int INCORRECT_ARGS_EXIT = 1;
+    private static final int SYNTAX_ERROR_EXIT = 100;
 
     public static void main(String[] args) {
         // Test arguments are correct
@@ -26,14 +30,28 @@ public class WACCCompiler {
         try {
             CharStream input = CharStreams.fromFileName(file);
             WACCLexer lexer = new WACCLexer(input);
-            CommonTokenStream tokens = new CommonTokenStream(lexer);
+            lexer.removeErrorListeners();
+            TokenStream tokens = new CommonTokenStream(lexer);
             WACCParser parser = new WACCParser(tokens);
+            parser.removeErrorListeners();
 
-            //ExampleVisitor visitor = new ExampleVisitor();
+            WACCParserErrorListener SyntaxErrorListener = new WACCParserErrorListener();
+            parser.addErrorListener(SyntaxErrorListener);
+
             WACCParser.ProgContext parseTree = parser.prog();
-            //visitor.visit(parseTree);
+
+            if (SyntaxErrorListener.hasError()) {
+                for (String error : SyntaxErrorListener.getSyntaxErrors()) {
+                    System.out.println(error);
+                }
+                System.exit(SYNTAX_ERROR_EXIT);
+            }
+
         } catch (IOException e) {
             System.err.println("File at path (" + file + ") doesn't exist");
+        } catch (RecognitionException e) {
+            System.err.println("Error with prediction, predicate failure or mismatched input occurred.");
         }
+
     }
 }
