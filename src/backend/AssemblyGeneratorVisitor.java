0f6b3e3598cdc25a7f0c729fa7d8784a6ece96f6
bunemekyakilika <kyakilika.b@gmail.com>
2019-02-21 11:44:12 +0000

    Added basic skeleton for backend

---
+++ src/backend/AssemblyGeneratorVisitor.java
@@ -0,0 +1,16 @@
+package backend;
+
+import antlr.WACCParserBaseVisitor;
+import backend.instructions.Instruction;
+
+import java.util.List;
+
+public class AssemblyGeneratorVisitor {
+
+    private List<Instruction> globalMainInstructions;
+
+
+
+
+
+}
---
+++ src/backend/instructions/Instruction.java
@@ -0,0 +1,4 @@
+package backend.instructions;
+
+public abstract class Instruction {
+}
