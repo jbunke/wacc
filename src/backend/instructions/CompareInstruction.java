e6f5dc576d797f2acb38b7b46c4b7e5a1245e9c0
bunemekyakilika <kyakilika.b@gmail.com>
2019-02-28 11:58:06 +0000

    Implemented multiplication

--- src/backend/instructions/CompareInstruction.java
+++ src/backend/instructions/CompareInstruction.java
@@ -1,24 +1,35 @@
 package backend.instructions;
 
-import backend.Condition;
 import backend.Register;
 
 public class CompareInstruction extends Instruction {
   private final Register operand1;
   private final String operand2;
+  private final String asrConst;
 
   public CompareInstruction(Register register, int operand) {
     this.operand1 = register;
     this.operand2 = "#" + Integer.toString(operand);
+    asrConst = null;
   }
 
   public CompareInstruction(Register operand1, Register operand2) {
     this.operand1 = operand1;
     this.operand2 = operand2.toString();
+    asrConst = null;
+  }
+
+  public CompareInstruction(Register operand1, Register operand2, int asrConst){
+    this.operand1 = operand1;
+    this.operand2 = operand2.toString();
+    this.asrConst = Integer.toString(asrConst);
   }
 
   @Override
   public String asString() {
+    if(asrConst != null) {
+      return "CMP " + operand1.toString() + ", " + operand2 + ", ASR #"+asrConst;
+    }
     return "CMP " + operand1.toString() + ", " + operand2;
   }
 }
