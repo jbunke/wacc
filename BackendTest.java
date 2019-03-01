9c4992a1d756b25dacd02af95ef5e7efc50881fe
km2717 <km2717@ic.ac.uk>
2019-03-01 14:47:45 +0000

    implemented stdinput handling for emulator tests, and expanded testsuite to cover more .wacc files

--- BackendTest.java
+++ BackendTest.java
@@ -15,21 +15,26 @@ import java.util.stream.Stream;
 public class BackendTest {
 
   private static final int FILENAME_ARG_INDEX = 0;
-  private static final int EXPECTED_NUM_ARGS = 1;
+  private static final int STDINPUT_ARG_INDEX = 1;
+  private static final int EXPECTED_NUM_ARGS = 2;
   private static final int TEST_FAILURE_EXIT = 1;
   private static final int TEST_SUCCESS_EXIT = 0;
 
   public static void main(String[] args) {
-    if (args.length != EXPECTED_NUM_ARGS) {
-      System.out.println("Expected number of args: " + EXPECTED_NUM_ARGS + "\n"
-          + "Number received: " + args.length);
-      System.exit(TEST_FAILURE_EXIT);
-    }
+    // if (args.length != EXPECTED_NUM_ARGS) {
+    //   System.out.println("Expected number of args: " + EXPECTED_NUM_ARGS + "\n"
+    //       + "Number received: " + args.length);
+      // System.exit(TEST_FAILURE_EXIT);
+    // }
 
     String filename = args[FILENAME_ARG_INDEX];
+    String stdinput = ""; 
+    if (args.length == 2){
+      stdinput = args[STDINPUT_ARG_INDEX];      
+    } 
 
     // Get the output from the assembly file our compiler generated
-    String emulatorOut = getEmulatorOutput(filename);
+    String emulatorOut = getEmulatorOutput(filename, stdinput);
 
     // Get the output from the reference compiler
     String refCompilerOut = getRefCompilerOutput(filename);
@@ -96,11 +101,11 @@ public class BackendTest {
     return outputString.toString();
   }
 
-  private static String getEmulatorOutput(String filename) {
+  private static String getEmulatorOutput(String filename, String stdinput) {
     StringBuilder outputString = new StringBuilder();
     String assemblyFile = filename.replaceFirst(".*/(\\w+).*", "$1");
     try {
-      Process process = Runtime.getRuntime().exec("bash tests/emulate " + assemblyFile);
+      Process process = Runtime.getRuntime().exec("bash tests/emulate " + assemblyFile + " " + stdinput);
       process.waitFor();
 
       BufferedReader reader =
